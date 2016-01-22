eb4227c9ad611a4caef900f783ebc7730b2d62ea
kimhyunsung <rlaakfkxhs@naver.com>
2016-01-22 16:41:23 +0900

    뭔가 바꼇다

--- P1_MapiaGame/src/com/sist/Test.java
+++ P1_MapiaGame/src/com/sist/Test.java
@@ -1,10 +1,15 @@
 package com.sist;
-
+import java.util.*;
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Çï·Î¿ì±ê");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b;
+		b=5;
+		System.out.printf("%d*%d=%d ÀÔ´Ï´Ù",a,b,a*b);
 	}
 
 }
