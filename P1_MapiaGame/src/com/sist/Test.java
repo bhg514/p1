284dfe759b8288cf5050938c74211fa0c7621dce
BaeHyunGil <bhk514@hanmil.net>
2016-01-22 16:26:08 +0900

    hell0 ,1234 추가

--- P1_MapiaGame/src/com/sist/Test.java
+++ P1_MapiaGame/src/com/sist/Test.java
@@ -4,7 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello");
+		System.out.println("12345678890");
+		System.out.println("Çï·Î¿ì ±ê");
 	}
 
 }
